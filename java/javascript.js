66dbee65d6f3144e5aa6ec8dc2a7008241e4899b
Knight <janae.jpeg@gmail.com>
2019-12-21 15:02:56 -0600

    gen update
    // hidden window,
    // lkned
    //
    
        /*allWindows.onload = createUI();
    
        function createUI() {
            allWindows.classList.add("lol");
            console.log("lol");
        }*/
    
    pseudo to add ui-widget-content class to all windows

--- java/javascript.js
+++ java/javascript.js
@@ -1,1 +1,59 @@
 // document.getElementById("...").addEventListener("click", myFunction);
+
+document.body.style.backgroundImage = "url('https://i.ibb.co/KbJQ1bQ/1999.jpg')";
+document.body.style.backgroundSize = "850px";
+
+// 128 for classic repeat
+// 300px for purple static
+// 850px for 1999
+
+
+
+
+// Close All Windows
+    var allWindows = document.querySelectorAll(".window");
+    document.getElementById("ts-4").addEventListener("click", closeWindows);
+    function closeWindows() {
+        //allWindows.style.display = "none";
+        //console.log("All active windows have been closed.");
+        console.log(allWindows);
+    }
+
+// 
+
+    /*allWindows.onload = createUI();
+
+    function createUI() {
+        allWindows.classList.add("lol");
+        console.log("lol");
+    }*/
+
+
+
+
+
+
+
+
+
+
+
+// CREDITS
+
+    /*
+
+    👏 http://osxdaily.com/2018/01/01/classic-mac-os-tiling-wallpapers/, classic wallpapers
+    👏 http://www.iconarchive.com/artist/iconfactory.html, pseudo os icons
+
+
+    */
+
+
+// IMPORTANT LINKS
+
+    /*
+
+    🔗 https://ibb.co/album/mrzrva, copland os wallpaper arch.
+
+
+    */
